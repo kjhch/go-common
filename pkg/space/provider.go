1bc5f925f09501f3ff21f75d467734b0a1ea929a
科君 <kejun.hch@qq.com>
2025-12-07 20:44:12 +0800

    feat: providers

--- pkg/space/provider.go
+++ pkg/space/provider.go
@@ -2,11 +2,11 @@ package space
 
 import "github.com/google/wire"
 
-var BaseProviderSet = wire.NewSet(NewApp, NewConfigLoader, NewLogger, startersProvider, NewGrpcServer, NewHttpServer)
+var BaseProviderSet = wire.NewSet(NewApp, NewConfigLoader, NewLogger, StartersProvider, NewGrpcServer, NewHttpServer)
 var KafkaProvider = wire.NewSet(NewKafkaListener)
 var NoKafkaProvider = wire.NewSet(wire.Value((*KafkaListener)(nil)))
 
-func startersProvider(grpcServer *GrpcServer, httpServer *HttpServer, listener *KafkaListener) []Starter {
+func StartersProvider(grpcServer *GrpcServer, httpServer *HttpServer, listener *KafkaListener) []Starter {
 	var starters []Starter
 	if grpcServer != nil {
 		starters = append(starters, grpcServer)
